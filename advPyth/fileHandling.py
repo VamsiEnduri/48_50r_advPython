740ce745b324c9aa7845f80a90e20746a8ff4eaa
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-11-21 23:23:40 +0530

    file handling

---
+++ advPyth/fileHandling.py
@@ -0,0 +1,67 @@
+# # a=open("vamsi.txt","w")
+# # a.write("vamsi is txt file created from filehandling file with open()")
+
+# # b=open("add.py","w")
+# # b.write("a=1000\nb=2000\nc=a+b\n")
+
+# # f=open("add.py","r")
+# # print(f.read())
+# # f.close()
+
+
+# # f=open("add.py","r")
+# # # print(f.readline())
+# # print(f.readlines())
+
+# # f=open("add.py","a")
+# # f.write("\nfruits=['mango','apple']")
+
+# # import os
+
+# # if os.path.exists("add.py"):
+# #     os.remove("add.py")
+# # f=open("vamsi.txt","r+")
+# # print(f.read())
+# # f.write("\ntoday file handling class via online")
+
+# # with open("decorators.py","r") as d:
+# #     for i in d:
+# #         print(i.strip())
+
+# # f=open("add.py","r")
+# # abc=f.read()
+# # print(abc)
+
+# # handling errors while doing file handling
+# import io
+# try:
+#     f=open("add.py","w")
+#     abc=f.write("a=10")
+#     print(abc)
+#     f=open("add.py","x")
+# except FileExistsError :
+#     print("file alredy existed create file with new name ")    
+# except  FileNotFoundError:
+#     print("try access r handle the file which exists...")  
+# except io.UnsupportedOperation :
+#     print("use proper operations according to mode of open")    
+# print("asdrftyj")    
+
+import os
+
+# os.mkdir("../pandas")
+# # open()
+# # os.rmdir("../pandas")
+
+# os.path.exists("")
+os.remove("Pandas")
+# os.rename("add.py","addition.py")
+
+# python runtime architecture
+
+pandas 2-3
+matplotlib - 1
+numpy -1
+
+
+
