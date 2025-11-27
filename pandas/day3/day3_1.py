c7dd13e807405e6961a1aa4ba47967447005b363
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2025-11-27 17:44:44 +0530

    pandas 3 day

---
+++ pandas/day3/day3_1.py
@@ -0,0 +1,110 @@
+import pandas as pd
+
+data = {
+    "name": [
+        "vamsi", "ravi", "sathwik", "roja", "priya",
+        "kiran", "bharath", "sneha", "teja", "anil",
+        "ram", "nikhil", "samatha", "bindu", "varun",
+        "sravani", "suresh", "madhu", "rajitha", "swetha"
+    ],
+    "age": [
+        20, 19, 22, 21, 18,
+        23, 17, 20, 22, 19,
+        18, 24, 21, 22, 23,
+        20, 19, 25, 18, 21
+    ],
+    "marks": [
+        450, 389, 410, 500, 430,
+        390, 250, 470, 480, 300,
+        410, 445, 350, 365, 495,
+        425, 385, 470, 390, 355
+    ],
+    "department": [
+        "CSE", "ECE", "MECH", "EEE", "CIVIL",
+        "CSE", "ECE", "EEE", "MECH", "IT",
+        "CSE", "MECH", "IT", "EEE", "CIVIL",
+        "ECE", "IT", "CSE", "EEE", "MECH"
+    ],
+    "phone": [
+        "98765", "9876", "987654", "98a7654", "987 654", 
+        "987650", "965432", "abc987", "98-7654", "987065",
+        "987651", "999999", "987600", "123987", "908765",
+        "98700", "98--765", "000000", "9876x4", "965 432"
+    ],
+    "city": [
+        "hyd", "hydreabad", "banglre", "cheni", "mumbaii",
+        "hydrebad", "bengluru", "chen", "hyd", "banglor",
+        "kadap", "vizag", "vijyawada", "hyd", "tirupathi",
+        "cheanni", "hyd", "mumbai", "benglore", "hydrabad"
+    ],
+    "scholarship": [
+        "true", 1, "t", "y", "YES",
+        False, True, False, True, 0,
+        True, False, "No", "no", "n",
+        "f", "False", "True", False, "0"
+    ],
+    "student_join_date": [
+        "2024-06-01"] * 20   # same date for all students
+}
+
+# 0 :-- "0" :--- 0
+df = pd.DataFrame(data)
+# df["city"]=df["city"].str.contains("hyd",case=False)
+df["city"]=df["city"].str.replace(r"^hyd.*","Hyderabad",case=False,regex=True)
+df["city"]=df["city"].str.replace(r"^che.*","Chennai",case=False,regex=True)
+df["city"]=df["city"].str.replace(r"^ban.*","Bangolore",case=False,regex=True)
+df["city"]=df["city"].str.replace(r"^ben.*","Bangolore",case=False,regex=True)
+df["city"]=df["city"].str.replace(r"^mum.*","Mumbai",case=False,regex=True)
+
+
+# df["scholarship"]=df["scholarship"].astype(int)
+# df["scholarship"]=df["scholarship"].map({True:1,False:0})
+def clean_bool(x):
+    c=str(x).strip().lower()
+    if c in ["true", "1", "t", "y", "yes"]:
+        return 1
+    if c in ["no", "no", "n",
+        "f", "false","0"]  :
+        return 0     
+df["scholarship"]=df["scholarship"].apply(clean_bool)
+
+# print(df)
+
+#one cilumn selecting'
+# print(df["name"])
+#multiple column
+# print(df[["name","scholarship"]]) # 
+
+# print(df.loc[4,["scholarship","name","city"]])
+# print(df.loc[4])
+
+# print(df.loc((df["scholarship"]==1)))
+
+# print(df.loc[(df["age"]>=17) & (df["city"]=="Hyderabad") & (df["scholarship"]==0)])
+
+# print(df.loc[((df["age"]>=17) & (df["city"]=="Hyderabad")) & (df["scholarship"]==0)])
+
+# print(df.iloc[0:11,2:5])
+# print(df.iloc[-1])
+
+# print(df.iloc[[0,7,17,19],[0,2,5]])
+
+# print(df.loc[df["scholarship"]==0])
+
+# print(df["scholarship"]==1)
+# print(df["age"]>=15)
+# math()
+
+
+# print(df.query("age>=17 and city in ('Hyderabad','Mumbai')"))
+
+# print(df[df["city"].isin(["Mumbai","Hyderabad"])])
+
+# print(df)
+df["student_join_date"]=pd.to_datetime(df["student_join_date"])
+print(df["student_join_date"])
+df["year"]=df["student_join_date"].dt.year #2025-10-5
+df["month"]=df["student_join_date"].dt.month #2025-10-5
+df["day"]=df["student_join_date"].dt.day #2025-10-5
+df["day_name"]=df["student_join_date"].dt.day_name() #2025-10-5
+print(df)
